22a191f27fec9a7cdf7a789b6ded1798c16470cf
Hensikt <bjornemmaneel@gmail.com>
2021-03-18 19:56:40 +0100

    Desktop Version Infosite homepage

---
+++ Infopage/index.php
@@ -0,0 +1,288 @@
+<php ?>
+    <!DOCTYPE html>
+    <html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+        <title>Robin Assistant</title>
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <a href="#"><img src="https://via.placeholder.com/160x50" class="logo img-responsive" alt="Robin Assistant"></a>
+            </div>
+            <div class="custom-select">
+                <select name="Tool" class="selection" id="Tool">
+                    <option value="Something">Something</option>
+                    <option value="goes">Goes</option>
+                    <option value="here">Here</option>
+                    <option value="ey">Ey</option>
+                </select>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php" class="nav-list">Home</a></li>
+                    <li><a href="info.php" class="nav-list">Who, how, what robin</a></li>
+                    <li><a href="profiel.php" class="nav-list-image"><img src="https://via.placeholder.com/50x50" class="img-responsive" alt="profile"></a></li>
+                </ul>
+            </nav>
+            <!-- <div class="profiel">
+                <img src="" class="img-responsive profiel" alt="Profiel siluette">
+            </div> -->
+
+            <div class="header-image">
+                <h1>Get Robin Assistant today <br> and start helping others.</h1>
+                <button class="button">Get Robin Assistant</button>
+            </div>
+            <div class="header-footer">
+                <h2>Robin assistant service, your daily assistant</h2>
+            </div>
+        </header>
+
+        <div class="flavour-text">
+            <p>Robin Assistant is a new assistive software solution for people with special needs and their carers in
+                performing everyday activities. It is used for different types of brain conditions: autism spectrum, Down
+                syndrome, Alzheimer's disease and various forms of dementia, and other sorts of brain injuries.</p>
+                <div class="flavour-image">
+                    <img src="https://via.placeholder.com/150" class="img-responsive" alt="Warmth">
+                    <h3>Warmth</h3>
+                </div>
+                <div class="flavour-image">
+                    <img src="https://via.placeholder.com/150" class="img-responsive" alt="Reliability">
+                    <h3>Reliability</h3>
+                </div>
+                <div class="flavour-image">
+                    <img src="https://via.placeholder.com/150" class="img-responsive" alt="Helpfulness">
+                    <h3>Helpfulness</h3>
+                </div>
+                <div class="flavour-image">
+                    <img src="https://via.placeholder.com/150" class="img-responsive" alt="Simplicity">
+                    <h3>Simplicity</h3>
+                </div>
+        </div>
+
+        <div class="information-block white">
+            <div class="information-subblock image">
+                <img src="https://via.placeholder.com/500" class="img-responsive" alt="Picture for information">
+            </div>
+            <div class="information-subblock text">
+                <h4>What is Robin Service?</h4>
+                <p>Robin is an assistive software solution that allows carers to set <br> up and plan daily activities for the ones they are taking care of. <br> Robin will then assist the individual in performing these activities <br>by providing step-by-step guidance, using images, <br>text and audio instructions.</p>
+                <button class="button information">Read more</button>
+            </div>
+        </div>
+        <div class="information-block coloured">
+            <div class="information-subblock text">
+                <h4>How does it work?</h4>
+                <p>Online dashboard</p>
+                <p>The career can access Robin through the website <br> <a href="#">www.Robinassists.me</a> from a computer or any other internet <br> enabled device and log in on their personal Dashboard. <br> There, they can program the person's tasks for the day, week <br> or even the entire month, with the option to modify them at <br> any time.</p>
+                <button class="button information">Read more</button>
+            </div>
+            <div class="information-subblock image">
+                <img src="https://via.placeholder.com/500" class="img-responsive" alt="Picture for information">
+            </div>
+        </div>
+        <div class="information-block white">
+            <div class="information-subblock image">
+                <img src="https://via.placeholder.com/500" class="img-responsive" alt="Picture for information">
+            </div>
+            <div class="information-subblock text">
+                <h4>Mobile app</h4>
+                <p>The activities programmed by the carer in the online dashboard <br> are sent to the individual's Robin mobile application. When it is <br> time to perform an activity, the app will notify the individual <br> through an alert or notification.</p>
+                <button class="button information">Read more</button>
+            </div>
+        </div>
+<div class="plans">
+    <h2>Plans</h2>
+        <div class="plan-cards">
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Family plan</h4>
+                    <h4 class="extra">Private users</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <h3>€ 000</h3>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <h3>€ 000</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Individual professional plan basic</h4>
+                    <h4>Professional users</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <h3>€ 50</h3>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <h3>€ 40</h3>
+                    </div> 
+                </div>
+            </div>
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Individual professional plan basic+</h4>
+                    <h4>Professional users</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <h3>€ 80</h3>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <h3>€ 70</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="plan-cards">
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Organisation <br> plan basic</h4>
+                    <h4>Professional organisations</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <h3>€ 200</h3>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <h3>€ 175</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Organisation <br> plan basic+</h4>
+                    <h4>Professional organisations</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <h3>€ 350</h3>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <h3>€ 300</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="plan-cards-container">
+                <div class="plan-cards-image">
+                    <img src="https://via.placeholder.com/150x50" alt="" class="logo img-responsive">
+                </div>
+                <div class="plan-cards-body">
+                    <h4>Organisation <br> plan CUSTOM</h4>
+                    <h4>Professional organisations</h4>
+                </div>
+                <div class="plan-cards-footer">
+                    <div class="plan-cards-footer-section">
+                        <p>Region 1 EU</p>
+                        <p><span>One off set up free + monthly fee<span></p>
+                    </div>
+                    <div class="plan-cards-footer-section">
+                        <p>Region 2 World</p>
+                        <p><span>One off set up free + monthly fee<span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="call-to-action">
+        <div class="call-to-text call-to-action-section">
+            <h2>Did we already convince you? Get Robin Assistant today and start helping others.</h2>
+        </div>
+        <div class="call-to-button call-to-action-section">
+            <button class="button call-to-action-button">Get Robin Assistant</button>
+        </div>
+    </div>
+
+    <div class="reviews-container">
+        <div class="reviews-slide fade">
+            <div class="numbertext">1 / 3</div>
+            <div class="reviews-text">
+                <h2>Reviews</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a molestie eros. Mauris risus erat, vulputate ut nunc eget, viverra mollis eros. Fusce ipsum dolor, semper at interdum ac, semper at sapien. Vestibulum sagittis, velit at auctor dictum, libero nisl pulvinar sapien, in fringilla nulla mauris egestas lacus. Nunc facilisis at purus vitae maximus. Proin in fermentum purus. Mauris commodo urna vel neque aliquet, vel viverra tortor lacinia. Pellentesque id ornare libero, eu mattis lacus. Donec vehicula vulputate augue, ac eleifend turpis lacinia id. Vestibulum quis sodales nibh, vel tempor mi. Proin arcu mauris, commodo id hendrerit eu, molestie vitae ipsum. Nam eu sollicitudin ex. Integer id fermentum metus, a dapibus nisi.</p>
+            </div>
+        </div>
+        <a class="prev">&#8592;</a>
+        <a class="next">&#8594;</a>
+    </div>
+
+    <div class="question">
+        <h2>Questions? Ask Robin!</h2>
+        <img src="https://via.placeholder.com/150" alt="" class="img-responsive aks-robin">
+    </div>
+
+        <footer>
+            <div class="footer-container">
+                <div class="footer-container-info address">
+                    <p>Bemika Software</p>
+                    <br>
+                    <address>
+                        <p>Waterman 134</p> 
+                        <p>1622 Cx</p> 
+                        <p>Hoorn</p>
+                    </address>
+                </div>
+                <div class="footer-container-info info">
+                    <p>Robin is an assistive software solution that allows carers to set up and plan daily activities for the ones they are taking care of.</p>
+                </div>
+                <div class="footer-container-info partners I">
+                    <h3>Partners</h3>
+                    <ul class="partners">
+                        <li>Mediacollege Amsterdam</li>
+                        <li>Serbian Autism Society</li>
+                        <li>Fonds Voor Communicatie Innovatie</li>
+                        
+                    </ul>
+                </div>
+                <div class="footer-container-info partners II">          
+                    <ul class="partners">
+                        <li>Foxico</li>
+                        <li>Bright Babes</li>
+                        <li>The Flying Dutchman</li>
+                        <li>Trademark Communicatie</li>
+                    </ul>
+                </div>
+                <div class="footer-container-info taal">
+                    <h2>Taal</h2>
+                    <select name="Taal" id="Taal" class="select">
+                        <option value="#">Taal</option>
+                        <option value="English">English</option>
+                        <option value="Nederlands">Nederlands</option>
+                    </select>
+                </div>
+            </div>
+        </footer>
+    </body>
+    </html>
